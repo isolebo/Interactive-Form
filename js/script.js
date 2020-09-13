3f4f2a94274e642722ec73288c3e9f7d0f05c2d0
Isiaq Solebo <i.solebo32@gmail.com>
2020-09-13 08:32:36 -0500

    Created jobTitleSelection
    When the user selects job title, and if they select other a new text field appears.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ function setDefaultFocus() {
     document.getElementById("name").focus()
 }
 setDefaultFocus();
+
+
+//if the user selects other then a new text field should appear
+const jobTitleSelection = document.querySelector("#title");
+const jobRole= document.getElementById("other-title");
+jobRole.style.display = "none";
+
+
+    jobTitleSelection.addEventListener("change", (e) => {
+        
+    if(jobTitleSelection.value === "other"){
+        jobRole.style.display = "block";
+    }
+    else{
+        jobRole.style.display = "none";
+    }
+});
+displayJobRole();
